bf70c951ea73cdd6e4a85f3ac75dfa283751b754
dikuw <michael@dikuw.com>
2018-01-01 08:55:28 -0600

    initial commit

---
+++ Weather/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="8654384b-9e96-46f7-a916-64c25386bf38" description="SharePoint Add-in Feature" featureId="8654384b-9e96-46f7-a916-64c25386bf38" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="Weather Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="eba1af90-f7b4-48aa-af19-cb3ca2470ba9" />
+    <projectItemReference itemId="ffe8be86-6cdd-4dc1-95d5-2c870867e4f4" />
+    <projectItemReference itemId="0bd9bc48-9842-42eb-9a7e-5275ec360806" />
+    <projectItemReference itemId="e0702c8d-2969-4b76-9f3c-831706aa4b49" />
+  </projectItems>
+</feature>
---
+++ Weather/Scripts/App.js
@@ -0,0 +1,57 @@
+﻿'use strict';
+
+ExecuteOrDelayUntilScriptLoaded(initializePage, "sp.js");
+
+function initializePage(){
+    $(document).ready(function () {
+        var openWeatherMapAPI = "";
+        var weatherId = 800;
+        var temp = 999;
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                openWeatherMapAPI = "https://api.openweathermap.org/data/2.5/weather?lat=" +
+                                     position.coords.latitude + "&lon=" + position.coords.longitude +
+                                     "&units=imperial&appid=88e5c6f83341295f393ce1edb1d37184";
+                fetchWeather(openWeatherMapAPI);
+            });
+        }
+
+        function fetchWeather(API) {
+            $.getJSON(API, function (json) {
+                temp = parseInt(json.main.temp);
+
+                if ($('input[name="degType"]:checked').val() === "F") {
+                    $("#temp").html(temp + " deg F");
+                } else {
+                    $("#temp").html(toCelsius(temp) + " deg C");
+                }
+
+                $("#location").html(json.name + ", " + json.sys.country);
+                $("#weathertype").html(json.weather[0].description);
+
+                weatherId = json.weather[0].id;
+
+                $("#weatherimage").html('');
+
+                if (weatherId >= 200 && weatherId < 550) {
+                    $("#weatherimage").addClass('wi wi-day-rain');
+                } else if (weatherId >= 600 && weatherId < 700) {
+                    $("#weatherimage").addClass('wi wi-day-snow');
+                } else if (weatherId >= 700 && weatherId < 800) {
+                    $("#weatherimage").addClass('wi wi-day-fog');
+                } else {
+                    $("#weatherimage").addClass('wi wi-day-sunny');
+                }
+            });
+        }
+
+        $("input[name='degType']").click(function () {
+            fetchWeather(openWeatherMapAPI);
+        });
+
+        function toCelsius(f) {
+            return parseInt((5 / 9) * (f - 32));
+        }
+    });
+}
